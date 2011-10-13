1dcc1a9ecdb2e303dbc60ec90ebf4c28628b70fe
Chris Curtis <cmc@tmbx.com>
2011-10-12 21:40:15 -0400

    (split-backend) Added skeleton for management app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LuciteBackend::Application.config.secret_token = '95e8f422aa4f9f08419b04652bf787a64fa249c883dc1a5e18db782bf745f215081ba7fd51b49b0c42ff65e599555c780f628d008ff71da624b2611a72349a7b'
