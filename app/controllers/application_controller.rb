c491e24574628c8b24e10d05a4e72f56264c87b8
Chris Curtis <cmc@tmbx.com>
2011-10-15 17:18:10 -0400

    (split-backend) added development-mode autoreloading of libs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include FrontendHelpers::Html5Helper
+
+  before_filter :_reload_libs, :if => :_reload_libs?
   
+  def _reload_libs
+    RELOAD_LIBS.each do |lib|
+      require_dependency lib
+    end
+  end
+  
+  def _reload_libs?
+    defined? RELOAD_LIBS
+  end
+    
 end
